6f10b59903c7c7c5f009ebf4e441abf98ec04faf
arunlogo <arunlogo96@gmail.com>
2022-05-06 15:37:59 +0530

    First commit

---
+++ src/test/java/com/reversal/service/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reversal.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
